05b9c7e75f3294995db3bb1627758d53d8df808b
warbonnet333 <warbonnet333@gmail.com>
2020-05-27 21:48:50 +0300

    added main part

--- api/contacts/contact.model.js
+++ api/contacts/contact.model.js
@@ -5,13 +5,28 @@ const contactSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  subscription: { type: String, required: true },
   password: { type: String, required: true },
-  token: { type: String, required: false, default: "" },
+  subscription: {
+    type: String,
+    enum: ["free", "pro", "premium"],
+    default: "free",
+  },
+  token: { type: String, required: false },
 });
 
-const contactModel = mongoose.model("contacts", contactSchema);
+async function findContactByEmail(email) {
+  return this.findOne({ email });
+}
 
-// contactModel.find().then((res) => console.log(res));
+async function updateToken(id, newToken) {
+  return this.findByIdAndUpdate(id, {
+    token: newToken,
+  });
+}
+
+contactSchema.statics.findContactByEmail = findContactByEmail;
+contactSchema.statics.updateToken = updateToken;
+
+const contactModel = mongoose.model("contacts", contactSchema);
 
 module.exports = contactModel;
